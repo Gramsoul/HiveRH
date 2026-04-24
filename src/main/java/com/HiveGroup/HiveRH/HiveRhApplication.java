8c697d9d152034e278c125676518c4129e47fde5
Gonzalo <erebea@live.com.ar>
2026-04-24 19:05:35 -0300

    primer commit

---
+++ src/main/java/com/HiveGroup/HiveRH/HiveRhApplication.java
@@ -0,0 +1,13 @@
+package com.HiveGroup.HiveRH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiveRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiveRhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HiveGroup/HiveRH/HiveRhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HiveGroup.HiveRH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiveRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
