5822c811148a495a0765cfff319402515aab01fe
VictorM-Herrera <victormherrerac14@gmail.com>
2026-05-26 19:02:05 -0300

    GlobalExceptionHandler

--- src/main/java/com/HiveGroup/HiveRH/Features/Position/PositionNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.HiveGroup.HiveRH.Features.Position;
-
-public class PositionNotFoundException extends RuntimeException {
-    public PositionNotFoundException(String message) {
-        super(message);
-    }
-}
