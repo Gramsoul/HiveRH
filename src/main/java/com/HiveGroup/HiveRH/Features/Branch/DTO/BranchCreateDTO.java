323c34b576f7beed0c5604411e13b30f44820f7b
Jose Luis Romero Rajoy <jl.romerorajoy@gmail.com>
2026-06-12 12:31:52 -0300

    Agregar validaciones branch

--- src/main/java/com/HiveGroup/HiveRH/Features/Branch/DTO/BranchCreateDTO.java
+++ src/main/java/com/HiveGroup/HiveRH/Features/Branch/DTO/BranchCreateDTO.java
@@ -1,11 +1,21 @@
 package com.HiveGroup.HiveRH.Features.Branch.DTO;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record BranchCreateDTO(
         @JsonAlias("branchName")
+        @NotBlank(message = "El nombre de la sucursal es obligatorio")
+        @Size(max = 100, message = "El nombre de la sucursal no puede superar los 100 caracteres")
         String name,
+
+        @NotBlank(message = "La ciudad es obligatoria")
+        @Size(max = 100, message = "La ciudad no puede superar los 100 caracteres")
         String city,
+
+        @NotBlank(message = "La direccion es obligatoria")
+        @Size(max = 100, message = "La direcciÃ³n no puede superar los 100 caracteres")
         String address
 ) {
 }
