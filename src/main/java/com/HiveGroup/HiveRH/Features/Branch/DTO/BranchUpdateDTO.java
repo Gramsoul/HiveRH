051ca6d153f85e2a2057b325d60ba9d330c74b81
Cristian <ncristian.molina@gmail.com>
2026-06-21 17:12:53 -0300

    Cambios Update

--- src/main/java/com/HiveGroup/HiveRH/Features/Branch/DTO/BranchUpdateDTO.java
+++ src/main/java/com/HiveGroup/HiveRH/Features/Branch/DTO/BranchUpdateDTO.java
@@ -6,16 +6,11 @@ import jakarta.validation.constraints.Size;
 
 public record BranchUpdateDTO(
         @JsonAlias("branchName")
-        @NotBlank(message = "El nombre de la sucursal es obligatorio")
-        @Size(max = 100, message = "El nombre de la sucursal no puede superar los 100 caracteres")
+
         String name,
 
-        @NotBlank(message = "La ciudad es obligatoria")
-        @Size(max = 100, message = "La ciudad no puede superar los 100 caracteres")
         String city,
 
-        @NotBlank(message = "La direccion es obligatoria")
-        @Size(max = 100, message = "La direcciÃ³n no puede superar los 100 caracteres")
         String address,
 
         Boolean active
